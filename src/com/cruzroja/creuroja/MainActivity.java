0b5c4b197dc4b9f2d6effd1946f608a4a7856e89
darthsuicune <darthsuicune@gmail.com>
2013-02-12 00:30:19 +0100

    Initial Commit

--- src/com/cruzroja/creuroja/MainActivity.java
+++ src/com/cruzroja/creuroja/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cruzroja.creuroja;
 
-import android.os.Bundle;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +12,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		SupportMapFragment fragment;
 	}
 
 	@Override
