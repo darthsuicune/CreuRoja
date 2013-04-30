66aa9f235b21f479a74781baa2945435ab552162
Denís <lapuente@abbyy.com>
2013-04-30 14:23:32 +0200

    Externalize strings for icons, just in case

--- src/com/cruzroja/creuroja/Location.java
+++ src/com/cruzroja/creuroja/Location.java
@@ -14,6 +14,14 @@ public class Location implements Serializable {
 	public static final String MARKER_STRONG = "<strong>";
 	public static final String MARKER_STRONG_END = "</strong>";
 	public static final String MARKER_SPACE = "&nbsp;";
+	
+	public static final String ICON_ASAMBLEA = "asamblea.png";
+	public static final String ICON_BRAVO = "bravo.png";
+	public static final String ICON_CUAP = "cuap.png";
+	public static final String ICON_EMBARCACION = "embarcacion.png";
+	public static final String ICON_HOSPITAL = "hospital.png";
+	public static final String ICON_PREVENTIVO = "preventivo.png";
+	public static final int NO_ICON = 0;
 
 	public double mLat;
 	public double mLong;
@@ -28,20 +36,20 @@ public class Location implements Serializable {
 	}
 
 	public int getIcon(String icon) {
-		if (icon.equals("asamblea.png")) {
+		if (icon.equals(ICON_ASAMBLEA)) {
 			return R.drawable.asamblea;
-		} else if (icon.equals("bravo.png")) {
+		} else if (icon.equals(ICON_BRAVO)) {
 			return R.drawable.bravo;
-		} else if (icon.equals("cuap.png")) {
+		} else if (icon.equals(ICON_CUAP)) {
 			return R.drawable.cuap;
-		} else if (icon.equals("embarcacion.png")) {
+		} else if (icon.equals(ICON_EMBARCACION)) {
 			return R.drawable.embarcacion;
-		} else if (icon.equals("hospital.png")) {
+		} else if (icon.equals(ICON_HOSPITAL)) {
 			return R.drawable.hospital;
-		} else if (icon.equals("preventivo.png")){
+		} else if (icon.equals(ICON_PREVENTIVO)){
 			return R.drawable.preventivo;
 		} else {
-			return 0;
+			return NO_ICON;
 		}
 	}
 
