8800832dff70890d4058600900f177cdf17d6c92
Denís <lapuente@abbyy.com>
2013-08-27 17:47:50 +0200

    Formatting issues

--- src/com/cruzroja/creuroja/CRMapActivity.java
+++ src/com/cruzroja/creuroja/CRMapActivity.java
@@ -17,118 +17,118 @@ import com.cruzroja.creuroja.utils.ConnectionClient;
 import com.cruzroja.creuroja.utils.Settings;
 
 public class CRMapActivity extends ActionBarActivity implements LoaderCallbacks<User> {
-	private static final int ACTIVITY_LOGIN = 1;
-	private static final int LOGIN_LOADER = 1;
+    private static final int ACTIVITY_LOGIN = 1;
+    private static final int LOGIN_LOADER = 1;
 
-	private SharedPreferences prefs;
+    private SharedPreferences prefs;
 
-	private CRMapFragment mMapFragment;
+    private CRMapFragment mMapFragment;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#CC0000")));
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#CC0000")));
 
-		if (savedInstanceState != null) {
-			return;
-		}
+        if (savedInstanceState != null) {
+            return;
+        }
 
-		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-		if (!prefs.getBoolean(Settings.IS_VALID_USER, false)) {
-			Intent intent = new Intent(this, LoginActivity.class);
-			startActivityForResult(intent, ACTIVITY_LOGIN);
-		} else {
-			checkCredentials();
-			showMap();
-		}
-	}
+        if (!prefs.getBoolean(Settings.IS_VALID_USER, false)) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivityForResult(intent, ACTIVITY_LOGIN);
+        } else {
+            checkCredentials();
+            showMap();
+        }
+    }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		switch (requestCode) {
-		case ACTIVITY_LOGIN:
-			switch (resultCode) {
-			case RESULT_OK:
-				showMap();
-				break;
-			default:
-				finish();
-				break;
-			}
-			break;
-		default:
-			break;
-		}
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+        case ACTIVITY_LOGIN:
+            switch (resultCode) {
+            case RESULT_OK:
+                showMap();
+                break;
+            default:
+                finish();
+                break;
+            }
+            break;
+        default:
+            break;
+        }
+    }
 
-	@Override
-	public void onBackPressed() {
-		if (mMapFragment != null && mMapFragment.isAdded()) {
-			if (!mMapFragment.onBackPressed()) {
-				super.onBackPressed();
-			}
-		} else {
-			super.onBackPressed();
-		}
-	}
+    @Override
+    public void onBackPressed() {
+        if (mMapFragment != null && mMapFragment.isAdded()) {
+            if (!mMapFragment.onBackPressed()) {
+                super.onBackPressed();
+            }
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-	private void showMap() {
+    private void showMap() {
 
-		mMapFragment = (CRMapFragment) Fragment.instantiate(this, CRMapFragment.class.getName());
-		mMapFragment.setHasOptionsMenu(true);
-		mMapFragment.setRetainInstance(true);
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.map_fragment_container, mMapFragment).commit();
-	}
+        mMapFragment = (CRMapFragment) Fragment.instantiate(this, CRMapFragment.class.getName());
+        mMapFragment.setHasOptionsMenu(true);
+        mMapFragment.setRetainInstance(true);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.map_fragment_container, mMapFragment).commit();
+    }
 
-	/*************************
-	 * Login related methods *
-	 *************************/
+    /*************************
+     * Login related methods *
+     *************************/
 
-	private void checkCredentials() {
-		if (ConnectionClient.isConnected(this)) {
-			Bundle args = new Bundle();
-			args.putString(Settings.USERNAME, prefs.getString(Settings.USERNAME, ""));
-			args.putString(Settings.PASSWORD, prefs.getString(Settings.PASSWORD, ""));
-			getSupportLoaderManager().restartLoader(LOGIN_LOADER, args, this);
-		}
-	}
+    private void checkCredentials() {
+        if (ConnectionClient.isConnected(this)) {
+            Bundle args = new Bundle();
+            args.putString(Settings.USERNAME, prefs.getString(Settings.USERNAME, ""));
+            args.putString(Settings.PASSWORD, prefs.getString(Settings.PASSWORD, ""));
+            getSupportLoaderManager().restartLoader(LOGIN_LOADER, args, this);
+        }
+    }
 
-	@Override
-	public Loader<User> onCreateLoader(int id, Bundle args) {
-		if (ConnectionClient.isConnected(this)) {
-			return new LoginLoader(CRMapActivity.this, args);
-		} else {
-			return null;
-		}
-	}
+    @Override
+    public Loader<User> onCreateLoader(int id, Bundle args) {
+        if (ConnectionClient.isConnected(this)) {
+            return new LoginLoader(CRMapActivity.this, args);
+        } else {
+            return null;
+        }
+    }
 
-	@Override
-	public void onLoadFinished(Loader<User> loader, User result) {
-		if (result != null) {
-			if (!result.mName.equals("")) {
-				// Valid user
-				result.save(prefs);
-				mMapFragment.setUser(result);
-				return;
-			} else { 
-				// The user has been invalidated by the system administrator.
-				Settings.clean(this);
-				Toast.makeText(this, R.string.error_user_invalidated, Toast.LENGTH_LONG).show();
-				finish();
-			}
+    @Override
+    public void onLoadFinished(Loader<User> loader, User result) {
+        if (result != null) {
+            if (!result.mName.equals("")) {
+                // Valid user
+                result.save(prefs);
+                mMapFragment.setUser(result);
+                return;
+            } else {
+                // The user has been invalidated by the system administrator.
+                Settings.clean(this);
+                Toast.makeText(this, R.string.error_user_invalidated, Toast.LENGTH_LONG).show();
+                finish();
+            }
 
-		} else {
-			// There was a problem with the connection or with the server. Usage is allowed.
-			Log.e(Settings.LOG, getString(R.string.error_connection));
-		}
-	}
+        } else {
+            // There was a problem with the connection or with the server. Usage is allowed.
+            Log.e(Settings.LOG, getString(R.string.error_connection));
+        }
+    }
 
-	@Override
-	public void onLoaderReset(Loader<User> loader) {
-	}
+    @Override
+    public void onLoaderReset(Loader<User> loader) {
+    }
 }
--- src/com/cruzroja/creuroja/utils/ConnectionClient.java
+++ src/com/cruzroja/creuroja/utils/ConnectionClient.java
@@ -34,225 +34,224 @@ import com.google.android.gms.maps.model.LatLng;
  * Petition requirements: -Send via POST -Set content-type header to "text/xml" or "application/xml"
  */
 public class ConnectionClient {
-	// URLs
-	public static final String BASE_URL = "http://r0uzic.net/voluntarios/";
-	public static final String URL_LOGIN = BASE_URL + "android/user/login";
-	public static final String URL_PUNTOS = BASE_URL + "map/marcadores.json";
+    // URLs
+    public static final String BASE_URL = "http://r0uzic.net/voluntarios/";
+    public static final String URL_LOGIN = BASE_URL + "android/user/login";
+    public static final String URL_PUNTOS = BASE_URL + "map/marcadores.json";
 
-	public static final String DIRECTIONS_API_BASE_URL = "https://maps.googleapis.com/maps/api/directions/json?region=es&";
+    public static final String DIRECTIONS_API_BASE_URL = "https://maps.googleapis.com/maps/api/directions/json?region=es&";
 
-	public static final String ORIGIN_URL = "origin=";
-	public static final String DESTINATION_URL = "destination=";
-	public static final String SENSOR_URL = "sensor=";
+    public static final String ORIGIN_URL = "origin=";
+    public static final String DESTINATION_URL = "destination=";
+    public static final String SENSOR_URL = "sensor=";
 
-	// XML preconstructed strings
-	public static final String XML_PETITION_1 = "<?xml version=\"1.0\"?>\n" + "<methodCall>\n"
-			+ "   <methodName>user.login</methodName>\n" + "   <params>\n" + "     <param>\n"
-			+ "        <value><string>";
-	public static final String XML_PETITION_2 = "</string></value>\n" + "     </param>\n"
-			+ "    <param>\n" + "        <value><string>";
-	public static final String XML_PETITION_3 = "</string></value>\n" + "     </param>\n"
-			+ "   </params>\n" + "</methodCall>";
-	public static final String CONTENT_TYPE = "content-type";
-	public static final String CONTENT_TYPE_XML = "text/xml";
+    // XML preconstructed strings
+    public static final String XML_PETITION_1 = "<?xml version=\"1.0\"?>\n" + "<methodCall>\n"
+            + "   <methodName>user.login</methodName>\n" + "   <params>\n" + "     <param>\n"
+            + "        <value><string>";
+    public static final String XML_PETITION_2 = "</string></value>\n" + "     </param>\n"
+            + "    <param>\n" + "        <value><string>";
+    public static final String XML_PETITION_3 = "</string></value>\n" + "     </param>\n"
+            + "   </params>\n" + "</methodCall>";
+    public static final String CONTENT_TYPE = "content-type";
+    public static final String CONTENT_TYPE_XML = "text/xml";
 
-	// This class should not be instantiated
-	private ConnectionClient() {
-	}
+    // This class should not be instantiated
+    private ConnectionClient() {
+    }
 
-	/**
-	 * * Method to call to perform a credentials validation.
-	 * 
-	 * @param username
-	 * @param password
-	 * @return null with server error. User with "" as name for invalid credentials. User with
-	 *         parameters for valid user.
-	 */
-	public static User doLogin(String username, String password) {
-		return parseLoginResponse(makeConnection(buildLoginRequest(username, password)));
-	}
+    /**
+     * * Method to call to perform a credentials validation.
+     * 
+     * @param username
+     * @param password
+     * @return null with server error. User with "" as name for invalid credentials. User with
+     *         parameters for valid user.
+     */
+    public static User doLogin(String username, String password) {
+        return parseLoginResponse(makeConnection(buildLoginRequest(username, password)));
+    }
 
-	/**
-	 * 
-	 * @param context
-	 * @return
-	 */
-	public static ArrayList<Location> downloadLocations(Context context) {
-		return parseLocationsResponse(context, makeConnection(new HttpGet(URL_PUNTOS)));
-	}
+    /**
+     * 
+     * @param context
+     * @return
+     */
+    public static ArrayList<Location> downloadLocations(Context context) {
+        return parseLocationsResponse(context, makeConnection(new HttpGet(URL_PUNTOS)));
+    }
 
-	public static ArrayList<LatLng> getDirections(double originLatitud, double originLongitud,
-			double destinationLatitud, double destinationLongitud) {
-		return parseDirectionsResponse(makeConnection(buildDirectionsRequest(originLatitud,
-				originLongitud, destinationLatitud, destinationLongitud)));
-	}
+    public static ArrayList<LatLng> getDirections(double originLatitud, double originLongitud,
+            double destinationLatitud, double destinationLongitud) {
+        return parseDirectionsResponse(makeConnection(buildDirectionsRequest(originLatitud,
+                originLongitud, destinationLatitud, destinationLongitud)));
+    }
 
-	/*******************
-	 * Utility methods *
-	 *******************/
+    /*******************
+     * Utility methods *
+     *******************/
 
-	/**
-	 * Should be always called before using the internet connection.
-	 * 
-	 * @param context
-	 * @return
-	 */
-	public static boolean isConnected(Context context) {
-		ConnectivityManager cm = (ConnectivityManager) context
-				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		if (cm.getActiveNetworkInfo() != null) {
-			return cm.getActiveNetworkInfo().isConnected();
-		}
+    /**
+     * Should be always called before using the internet connection.
+     * 
+     * @param context
+     * @return
+     */
+    public static boolean isConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm.getActiveNetworkInfo() != null) {
+            return cm.getActiveNetworkInfo().isConnected();
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	private static HttpResponse makeConnection(HttpUriRequest request) {
-		if (request == null) {
-			return null;
-		}
-		DefaultHttpClient httpClient = new DefaultHttpClient();
-		
+    private static HttpResponse makeConnection(HttpUriRequest request) {
+        if (request == null) {
+            return null;
+        }
+        DefaultHttpClient httpClient = new DefaultHttpClient();
 
-		try {
-			return httpClient.execute(request);
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        try {
+            return httpClient.execute(request);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**************************
-	 * Login specific methods *
-	 **************************/
+    /**************************
+     * Login specific methods *
+     **************************/
 
-	private static HttpUriRequest buildLoginRequest(String username, String password) {
-		HttpPost request = new HttpPost(URL_LOGIN);
-		request.setHeader(new BasicHeader(CONTENT_TYPE, CONTENT_TYPE_XML));
-		try {
-			request.setEntity(new StringEntity(XML_PETITION_1 + username + XML_PETITION_2
-					+ password + XML_PETITION_3));
-		} catch (UnsupportedEncodingException e) {
-			return null;
-		}
-		return request;
-	}
+    private static HttpUriRequest buildLoginRequest(String username, String password) {
+        HttpPost request = new HttpPost(URL_LOGIN);
+        request.setHeader(new BasicHeader(CONTENT_TYPE, CONTENT_TYPE_XML));
+        try {
+            request.setEntity(new StringEntity(XML_PETITION_1 + username + XML_PETITION_2
+                    + password + XML_PETITION_3));
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
+        return request;
+    }
 
-	private static User parseLoginResponse(HttpResponse response) {
-		if (response != null && response.getStatusLine() != null) {
-			switch (response.getStatusLine().getStatusCode()) {
-			case 200:
-				// Success in the connection. Check response
-				try {
-					return new User(response.getEntity().getContent());
-				} catch (IllegalStateException e) {
-					e.printStackTrace();
-					return null;
-				} catch (IOException e) {
-					e.printStackTrace();
-					return null;
-				}
-			default:
-				return null;
-			}
-		} else {
-			return null;
-		}
-	}
+    private static User parseLoginResponse(HttpResponse response) {
+        if (response != null && response.getStatusLine() != null) {
+            switch (response.getStatusLine().getStatusCode()) {
+            case 200:
+                // Success in the connection. Check response
+                try {
+                    return new User(response.getEntity().getContent());
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                    return null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            default:
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
 
-	/*****************************
-	 * Location specific methods *
-	 *****************************/
+    /*****************************
+     * Location specific methods *
+     *****************************/
 
-	private static ArrayList<Location> parseLocationsResponse(Context context, HttpResponse response) {
-		ArrayList<Location> result = null;
-		if (response.getStatusLine() != null) {
-			switch (response.getStatusLine().getStatusCode()) {
-			case 200:
-				// Get everything as a string ready to parse and save later to
-				// disk.
-				BufferedReader reader;
-				try {
-					reader = new BufferedReader(new InputStreamReader(response.getEntity()
-							.getContent()));
+    private static ArrayList<Location> parseLocationsResponse(Context context, HttpResponse response) {
+        ArrayList<Location> result = null;
+        if (response.getStatusLine() != null) {
+            switch (response.getStatusLine().getStatusCode()) {
+            case 200:
+                // Get everything as a string ready to parse and save later to
+                // disk.
+                BufferedReader reader;
+                try {
+                    reader = new BufferedReader(new InputStreamReader(response.getEntity()
+                            .getContent()));
 
-					String data = "";
-					String line;
-					while ((line = reader.readLine()) != null) {
-						data += line;
-					}
-					if (reader != null) {
-						reader.close();
-					}
-					// Parse the resulting string. Save to disk if its correct
-					result = JSONParser.parseLocations(data);
-					if (result != null && !result.isEmpty()) {
-						FileUtils.writeToFile(context, data);
-					}
-				} catch (IllegalStateException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				break;
-			default:
-				break;
-			}
-		}
-		return result;
-	}
+                    String data = "";
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        data += line;
+                    }
+                    if (reader != null) {
+                        reader.close();
+                    }
+                    // Parse the resulting string. Save to disk if its correct
+                    result = JSONParser.parseLocations(data);
+                    if (result != null && !result.isEmpty()) {
+                        FileUtils.writeToFile(context, data);
+                    }
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                break;
+            }
+        }
+        return result;
+    }
 
-	/******************************
-	 * Direction specific methods *
-	 ******************************/
+    /******************************
+     * Direction specific methods *
+     ******************************/
 
-	private static HttpUriRequest buildDirectionsRequest(double originLatitud,
-			double originLongitud, double destinationLatitud, double destinationLongitud) {
-		return new HttpGet(URI.create(DIRECTIONS_API_BASE_URL + ORIGIN_URL + originLatitud + ","
-				+ originLongitud + "&" + DESTINATION_URL + destinationLatitud + ","
-				+ destinationLongitud + "&" + SENSOR_URL + getSensorAvailability()));
-	}
+    private static HttpUriRequest buildDirectionsRequest(double originLatitud,
+            double originLongitud, double destinationLatitud, double destinationLongitud) {
+        return new HttpGet(URI.create(DIRECTIONS_API_BASE_URL + ORIGIN_URL + originLatitud + ","
+                + originLongitud + "&" + DESTINATION_URL + destinationLatitud + ","
+                + destinationLongitud + "&" + SENSOR_URL + getSensorAvailability()));
+    }
 
-	// Placeholder in case we want to introduce sensor detection
-	private static String getSensorAvailability() {
-		return "true";
-	}
+    // Placeholder in case we want to introduce sensor detection
+    private static String getSensorAvailability() {
+        return "true";
+    }
 
-	private static ArrayList<LatLng> parseDirectionsResponse(HttpResponse response) {
-		ArrayList<LatLng> result = null;
-		if (response.getStatusLine() != null) {
-			switch (response.getStatusLine().getStatusCode()) {
-			case 200:
-				// Get everything as a string ready to parse
-				BufferedReader reader;
-				try {
-					reader = new BufferedReader(new InputStreamReader(response.getEntity()
-							.getContent()));
+    private static ArrayList<LatLng> parseDirectionsResponse(HttpResponse response) {
+        ArrayList<LatLng> result = null;
+        if (response.getStatusLine() != null) {
+            switch (response.getStatusLine().getStatusCode()) {
+            case 200:
+                // Get everything as a string ready to parse
+                BufferedReader reader;
+                try {
+                    reader = new BufferedReader(new InputStreamReader(response.getEntity()
+                            .getContent()));
 
-					String data = "";
-					String line;
-					while ((line = reader.readLine()) != null) {
-						data += line;
-					}
-					if (reader != null) {
-						reader.close();
-					}
-					// Parse the resulting string. Save to disk if its correct
-					result = JSONParser.parseDirections(data);
-				} catch (IllegalStateException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				break;
-			default:
-				break;
-			}
-		}
-		return result;
-	}
+                    String data = "";
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        data += line;
+                    }
+                    if (reader != null) {
+                        reader.close();
+                    }
+                    // Parse the resulting string. Save to disk if its correct
+                    result = JSONParser.parseDirections(data);
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                break;
+            }
+        }
+        return result;
+    }
 
 }
