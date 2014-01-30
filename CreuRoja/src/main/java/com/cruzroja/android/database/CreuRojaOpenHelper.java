80ae228223ddc59854c61ea4980af07ddc63dbf9
Denís <lapuente@abbyy.com>
2014-01-30 17:51:13 +0100

    Add not null to expire date.

--- CreuRoja/src/main/java/com/cruzroja/android/database/CreuRojaOpenHelper.java
+++ CreuRoja/src/main/java/com/cruzroja/android/database/CreuRojaOpenHelper.java
@@ -29,7 +29,7 @@ public class CreuRojaOpenHelper extends SQLiteOpenHelper {
                 + CreuRojaContract.Locations.NAME + " TEXT NOT NULL, "
                 + CreuRojaContract.Locations.ADDRESS + " TEXT, "
                 + CreuRojaContract.Locations.DETAILS + " TEXT, "
-                + CreuRojaContract.Locations.EXPIRE_DATE + " INTEGER, "
+                + CreuRojaContract.Locations.EXPIRE_DATE + " INTEGER NOT NULL, "
                 + CreuRojaContract.Locations.LAST_MODIFIED + " INTEGER NOT NULL)"
         );
 
