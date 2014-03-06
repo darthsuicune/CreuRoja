2ed6b27087267604fa41c6597c58925b0b01f251
Denis Lapuente <darthsuicune@gmail.com>
2014-03-07 00:28:12 +0100

    Start migration from username to e-mail

--- CreuRoja/src/main/java/com/cruzroja/android/app/loaders/LoginLoader.java
+++ CreuRoja/src/main/java/com/cruzroja/android/app/loaders/LoginLoader.java
@@ -3,23 +3,18 @@ package com.cruzroja.android.app.loaders;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.content.AsyncTaskLoader;
-import android.util.Log;
 
 import com.cruzroja.android.R;
 import com.cruzroja.android.app.LoginResponse;
-import com.cruzroja.android.app.Settings;
 import com.cruzroja.android.app.utils.ConnectionClient;
-import com.cruzroja.android.app.utils.HashGenerator;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by lapuente on 27.11.13.
  */
 public class LoginLoader extends AsyncTaskLoader<LoginResponse> {
-    public static final String USERNAME = "Username";
+    public static final String EMAIL = "e-mail";
     public static final String PASSWORD = "Password";
 
     private ConnectionClient mClient;
@@ -27,7 +22,7 @@ public class LoginLoader extends AsyncTaskLoader<LoginResponse> {
 
     public LoginLoader(Context context, Bundle args) {
         super(context);
-        mUsername = args.getString(USERNAME);
+        mUsername = args.getString(EMAIL);
         mPassword = args.getString(PASSWORD);
     }
 
