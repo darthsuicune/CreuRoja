4b09dd73041f7afa972868f3534f2620b1b360d1
Denís <lapuente@abbyy.com>
2014-06-18 16:54:59 +0200

    Start login process

--- CreuRoja/src/main/java/com/cruzroja/android/model/ws/CRWebServiceClient.java
+++ CreuRoja/src/main/java/com/cruzroja/android/model/ws/CRWebServiceClient.java
@@ -1,9 +1,12 @@
 package com.cruzroja.android.model.ws;
 
+import android.util.Log;
+
 import com.cruzroja.android.model.Location;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
@@ -11,31 +14,40 @@ import org.apache.http.params.BasicHttpParams;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 /**
  * Created by lapuente on 18.06.14.
  */
 public class CRWebServiceClient implements WebServiceClient {
+	public static final String ARG_EMAIL = "email";
+	public static final String ARG_PASS = "password";
+	public static final String ARG_ACCESS_TOKEN = "access_token";
+	public static final String ARG_LAST_UPDATE = "last_update";
+
 	private static final String WS_PROTOCOL = "http";
 	private static final String WS_URL = "kls.servequake.com";
 	private static final String WS_PORT = "808";
-	private static final String WS_RESOURCE_LOGIN = "";
-	private static final String WS_RESOURCE_LOCATIONS = "";
+	private static final String WS_CONNECTION_POINT = "webservice.php";
+	private static final String WS_RESOURCE_LOGIN = "?q=request_access";
+	private static final String WS_RESOURCE_LOCATIONS = "?q=get_locations";
 
 	@Override
 	public String signInUser(String username, String password) {
 		HttpClient client = getHttpClient();
-		HttpUriRequest request = getHttpRequest();
 		try {
+			HttpUriRequest request = getLoginRequest();
 			HttpResponse response = client.execute(request);
-			StringBuilder builder = new StringBuilder();
-			BufferedReader reader =
-					new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-
+			String result = readResponse(response);
 			return null;
 		} catch (IOException e) {
 			e.printStackTrace();
+			return null;
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			Log.d("WSClient", "Incorrect URL");
 			return null;
 		}
 	}
@@ -56,7 +68,22 @@ public class CRWebServiceClient implements WebServiceClient {
 		return client;
 	}
 
-	private HttpUriRequest getHttpRequest() {
-		return null;
+	private HttpUriRequest getLoginRequest() throws URISyntaxException {
+		HttpPost post = new HttpPost();
+		StringBuilder builder = new StringBuilder();
+		URI uri = new URI(builder.toString());
+		post.setURI(uri);
+		return post;
+	}
+
+	private String readResponse(HttpResponse response) throws IOException {
+		StringBuilder builder = new StringBuilder();
+		BufferedReader reader =
+				new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			builder.append(line);
+		}
+		return line;
 	}
 }
