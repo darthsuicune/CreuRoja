126bd2d5e01dc47b570439e1b0e2b22cc20aba48
Denís <lapuente@abbyy.com>
2014-06-26 17:18:22 +0200

    Add more stuff

--- CreuRoja/src/main/java/org/creuroja/android/view/fragments/NavigationDrawerFragment.java
+++ CreuRoja/src/main/java/org/creuroja/android/view/fragments/NavigationDrawerFragment.java
@@ -100,15 +100,23 @@ public class NavigationDrawerFragment extends Fragment {
 				selectItem(position);
 			}
 		});
-		mDrawerListView.setAdapter(new ArrayAdapter<String>(getActionBar().getThemedContext(),
+		mDrawerListView.setAdapter(new ArrayAdapter<>(getActionBar().getThemedContext(),
 				android.R.layout.simple_list_item_activated_1, android.R.id.text1, new String[]{
 				getString(R.string.title_section1),
 				getString(R.string.title_section2),
 				getString(R.string.title_section3),}
 		));
 		mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
+		prepareLegend(v);
+		prepareMapTypes(v);
 		return v;
 
+	}
+
+	private void prepareLegend(View v) {
+	}
+
+	private void prepareMapTypes(View v){
 	}
 
 	public boolean isDrawerOpen() {
