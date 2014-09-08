33463ea96bc0299540db362a9d17118fde2e7475
Denis Lapuente <darthsuicune@gmail.com>
2014-09-08 20:37:01 +0200

    Add restrictions to vehicle position pushing

--- web/app/controllers/vehicle_positions_controller.rb
+++ web/app/controllers/vehicle_positions_controller.rb
@@ -1,4 +1,7 @@
 class VehiclePositionsController < ApplicationController
+	before_filter :signed_in_user
+	before_filter :is_valid_user
+	
 	def index
 		positions = VehiclePosition.select(:vehicle_id).distinct
 		@vehicles = []
@@ -25,5 +28,9 @@ class VehiclePositionsController < ApplicationController
 		params.require(:vehicle_id)
 		params.require(:latitude)
 		params.require(:longitude)
+	end
+	
+	def is_valid_user
+		redirect_to root_url unless current_user && current_user.allowed_to?(:manage_vehicles)
 	end
 end
