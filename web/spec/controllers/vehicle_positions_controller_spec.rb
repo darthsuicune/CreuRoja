858b0138f256c928bd23412cf4e7d05de296e81c
Denis Lapuente <darthsuicune@gmail.com>
2014-09-08 23:39:23 +0200

    Remove temp files

--- web/spec/controllers/vehicle_positions_controller_spec.rb
+++ web/spec/controllers/vehicle_positions_controller_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe VehiclePositionsController, :type => :controller do
 
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to be_success
-    end
-  end
+	describe "GET index" do
+		it "returns http success" do
+			get :index
+			expect(response).to be_success
+		end
+	end
 
-  describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to be_success
-    end
-  end
+	describe "GET create" do
+		it "returns http success" do
+			get :create
+			expect(response).to be_success
+		end
+	end
 
 end
