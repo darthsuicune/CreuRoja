0798ac19dd07b11fa41f476e8f6d171478778fd4
Denis Lapuente <darthsuicune@gmail.com>
2014-09-08 23:37:12 +0200

    Fix vehicle tracking

--- android/VehicleTracking/app/src/main/java/net/creuroja/android/vehicletracking/model/Vehicle.java
+++ android/VehicleTracking/app/src/main/java/net/creuroja/android/vehicletracking/model/Vehicle.java
@@ -5,16 +5,23 @@ import android.util.Log;
 import net.creuroja.android.vehicletracking.model.webservice.Auth;
 import net.creuroja.android.vehicletracking.model.webservice.ServerData;
 import net.creuroja.android.vehicletracking.model.webservice.lib.RestWebServiceClient;
-import net.creuroja.android.vehicletracking.model.webservice.lib.WebServiceFormat;
 import net.creuroja.android.vehicletracking.model.webservice.lib.WebServiceOption;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by lapuente on 04.09.14.
@@ -43,9 +50,7 @@ public class Vehicle {
 	public void upload(String accessToken) throws IOException {
 		RestWebServiceClient client = new RestWebServiceClient(RestWebServiceClient.PROTOCOL_HTTPS,
 				ServerData.SERVER_ADDRESS);
-		HttpResponse response =
-				client.post(ServerData.RESOURCE_NEW_VEHICLE_POSITION, WebServiceFormat.JSON,
-						getOptions(accessToken));
+		HttpResponse response = post(accessToken);
 		switch (response.getStatusLine().getStatusCode()) {
 			case 200:
 				Log.d("Something", "Something, something something");
@@ -57,20 +62,48 @@ public class Vehicle {
 
 	}
 
+	private HttpResponse post(String accessToken) throws IOException {
+		HttpClient client = new DefaultHttpClient();
+		return client.execute(getRequest(accessToken));
+	}
+
+	private HttpUriRequest getRequest(String accessToken) throws UnsupportedEncodingException {
+		HttpPost post = new HttpPost(
+				RestWebServiceClient.PROTOCOL_HTTPS + "://" + ServerData.SERVER_ADDRESS + "/" +
+				ServerData.RESOURCE_NEW_VEHICLE_POSITION + ".json");
+		setAuth(post, accessToken);
+		setParams(post);
+		return post;
+	}
+
+	private void setParams(HttpPost post) throws UnsupportedEncodingException {
+		String json = asJson().toString();
+		post.setEntity(new StringEntity(asJson().toString()));
+	}
+
+	private void setAuth(HttpPost post, String accessToken) {
+		String name = ServerData.ARG_ACCESS_TOKEN;
+		String value = "token=\"" + accessToken + "\"";
+		post.addHeader(name, value);
+		post.addHeader("Content-Type", "application/json");
+	}
+
 	private List<WebServiceOption> getOptions(String accessToken) {
 		List<WebServiceOption> options = new ArrayList<>();
 		options.add(Auth.getAuthOption(accessToken));
 		options.add(new WebServiceOption(WebServiceOption.OptionType.POST,
-				ServerData.ARG_VEHICLE_POSITION, this.asJson()));
+				ServerData.ARG_VEHICLE_POSITION, asJson().toString()));
 		return options;
 	}
 
-	private String asJson() {
-		return "{" +
-			   "\"" + ServerData.ARG_VEHICLE_ID + "\":\"" + id + "\","+
-			   "\"" + ServerData.ARG_VEHICLE_LATITUDE + "\":" + latitude + ","+
-			   "\"" + ServerData.ARG_VEHICLE_LONGITUDE + "\":" + longitude + ""+
-			   "}";
+	public JSONObject asJson() {
+		Map<String, Map<String, String>> holder = new HashMap<>();
+		Map<String, String> map = new HashMap<>();
+		map.put(ServerData.ARG_VEHICLE_ID, Integer.toString(id));
+		map.put(ServerData.ARG_VEHICLE_LATITUDE, Double.toString(latitude));
+		map.put(ServerData.ARG_VEHICLE_LONGITUDE, Double.toString(longitude));
+		holder.put(ServerData.ARG_VEHICLE_POSITION, map);
+		return new JSONObject(holder);
 	}
 
 	public String toString() {
