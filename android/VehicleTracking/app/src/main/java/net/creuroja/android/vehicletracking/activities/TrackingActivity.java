4246e7a2ee2a9f3c49722ad5bf8860762706d7f3
Denís <lapuente@abbyy.com>
2014-09-09 15:59:58 +0200

    Remove preference options

--- android/VehicleTracking/app/src/main/java/net/creuroja/android/vehicletracking/activities/TrackingActivity.java
+++ android/VehicleTracking/app/src/main/java/net/creuroja/android/vehicletracking/activities/TrackingActivity.java
@@ -12,8 +12,6 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
@@ -111,32 +109,6 @@ public class TrackingActivity extends ActionBarActivity
 						finish();
 				}
 		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.tracking, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		switch (item.getItemId()) {
-			case R.id.action_settings:
-				openSettings();
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-		}
-	}
-
-	private void openSettings() {
-		Intent intent = new Intent(this, SettingsActivity.class);
-		startActivity(intent);
 	}
 
 	@Override public void onTrackingStartRequested(Vehicle vehicle) {
