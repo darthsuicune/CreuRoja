87d2e65b3130a8324055ba1cdcbf8746feb7b7d0
Denís <lapuente@abbyy.com>
2014-09-11 16:24:37 +0200

    Small changes to test

--- web/spec/views/locations/index.json.jbuilder_spec.rb
+++ web/spec/views/locations/index.json.jbuilder_spec.rb
@@ -4,12 +4,13 @@ describe "locations/index" do
 	let(:user) { FactoryGirl.create(:user) }
 	let(:location) { FactoryGirl.create(:location) }
 	let(:locations) { [location] }
+	
 	before { 
 		sign_in user 
 		assign(:locations, [location])
 	}
 	
-	it "renders a valid json" do
+	it "renders the correct json" do
 		render template: "locations/index", formats: :json
 		
 		expect(rendered).to eq(locations.to_json except: [:created_at, :expiredate])
